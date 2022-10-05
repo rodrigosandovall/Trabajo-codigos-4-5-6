2ac9e0fb1cea8a980faf6376c001d2512ebf991a
Rodrigo Sandoval <69886884+rodrigosandovall@users.noreply.github.com>
2022-10-04 18:15:36 -0600

    Add files via upload

---
+++ revisión 4/codigo/src/codigo/Codigo4.java
@@ -0,0 +1,47 @@
+package codigo;
+
+import java.util.Scanner;
+
+public class Codigo4 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner s = new Scanner(System.in); 
+	    
+	    System.out.print("Turno del jugador 1 (introduzca piedra, papel o tijeras): ");
+	    String j1 = s.nextLine();
+	    
+	    
+	    System.out.print("Turno del jugador 2 (introduzca piedra, papel o tijeras): ");
+	    //Scanner s2 = new Scanner(System.in);
+	    String j2 = s.nextLine();
+	    
+	    if ((j1.equals(j2))) {
+	      System.out.println("Empate");
+	    } else { 
+	      int g = 2;
+	      switch(j1) {
+	        case "piedra":
+	          if (j2.equals("tijeras") ) {
+	            g = 1;
+	            }
+	          break;        
+	        case "papel":
+	          if (j2.equals("piedra") ) {
+	            g = 1;
+	          }
+	          break;
+	        case "tijera":
+	          if (j2.equals("papel")) {
+	            g = 1;
+	          }
+	          break;
+	        default:
+	        	
+	      }
+	      System.out.println("Gana el jugador " + g);
+	    }
+	 }
+}
+	  
+	
